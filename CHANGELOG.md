bd60e33ab47f9305a4b53fc1bb5a559a854d53fb
Hendrik Beneke <hendrik.beneke@gmail.com>
2016-03-05 16:01:20 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+0.0.1 / 2016-02-27
+==================
+- Initial commit of test utils
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const BaseTests = require('./lib/baseTests');
+const fixtures = require('./lib/fixtures');
+const Seed = require('./lib/seed');
+const initServer = require('./lib/initServer');
+
+module.exports = {
+  fixtures, BaseTests, Seed, initServer
+};
