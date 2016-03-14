1215317e7c073f83a5f19dfc3c3087f8afdbcace
Hendrik Beneke <hendrik.beneke@gmail.com>
2016-03-14 17:36:02 +0100

    added packages and scripts for tests; tests for inject util

---
+++ test/utils/inject.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const inject = require('../../lib/utils/inject');
+
+describe('inject.js', () => {
+  const server = {};
+
+  beforeEach('setup stubs', () => {
+    server.register = sinon.stub();
+  });
+
+  it('registers inject-then module with hapijs', () => {
+    server.plugins = {};
+    server.register.callsArg(1);
+    return inject(server)
+      .then(() => {
+        expect(server.register.calledOnce).to.be.true;
+      });
+  });
+
+  it('does not register inject-then module if it exists', () => {
+    server.plugins = {};
+    server.plugins['inject-then'] = {};
+    return inject(server)
+      .then(() => {
+        expect(server.register.called).to.be.false;
+      });
+  });
+});
