e2ea85d9022e0abfdcafecf3178c9b4fd6300f18
Hendrik Beneke <hendrik.beneke@gmail.com>
2016-04-29 13:05:38 +0200

    Expose db utils

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const BaseTests = require('./lib/baseTests');
 const fixtures = require('./lib/fixtures');
 const Seed = require('./lib/seed');
 const initServer = require('./lib/initServer');
+const db = require('./lib/utils/db');
 
 module.exports = {
-  fixtures, BaseTests, Seed, initServer
+  fixtures, BaseTests, Seed, initServer, db
 };
