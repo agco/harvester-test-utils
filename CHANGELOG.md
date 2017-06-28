7bbd5e44a4837977833caae283c5a109401af438
Hendrik Beneke <hendrik.beneke@gmail.com>
2017-06-29 01:09:47 +0200

    Fixed bug in inject util. Now it returns the server object even if inject-then was already registered.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.0.3 / 2017-06-29
+==================
+- Fixed bug in inject util. Now it returns the server object even if inject-then was already registered.
+
 0.0.2 / 2016-04-29
 ==================
 - Exposed DB utils
--- test/utils/inject.test.js
+++ test/utils/inject.test.js
@@ -15,8 +15,9 @@ describe('inject.js', () => {
     server.plugins = {};
     server.register.callsArg(1);
     return inject(server)
-      .then(() => {
+      .then(_serverReturn => {
         expect(server.register.calledOnce).to.be.true;
+        expect(_serverReturn).to.be.an('object');
       });
   });
 
@@ -24,8 +25,9 @@ describe('inject.js', () => {
     server.plugins = {};
     server.plugins['inject-then'] = {};
     return inject(server)
-      .then(() => {
+      .then(_serverReturn => {
         expect(server.register.called).to.be.false;
+        expect(_serverReturn).to.be.an('object');
       });
   });
 });
